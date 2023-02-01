e2ff456da85592fd11d51838056dc0e0a981fccb
abc <abc@example.com>
2023-01-31 20:24:07 -0500

    went thru with tutor #1 still doesnt work right

--- script.js
+++ script.js
@@ -12,49 +12,34 @@ let score = 0;
 const quizQs = [
     {
         question: "What year was the Boston Bruins most recent Stanley Cup win?",
-        a: "2013",
-        b: "2019",
-        c: "2011",
-        d: "2007",
-        answer: "c",
+        choices: ["2013", "2019", "2011", "2007"],
+        answer: "2011",
     },
     {
         question: "Who is the Bruins current Captain?",
-        a: "Bergeron",
-        b: "Chara",
-        c: "Orr",
-        d: "Pasta",
-        answer: "a",
+        choices: ["Bergeron", "Chara", "Orr", "Pasta"],
+        answer: "Bergeron",
     },
     {
         question: "What is the Bruins current standing (1-32) in the league as of January 30th 2023?",
-        a: "1st",
-        b: "16th",
-        c: "3rd",
-        d: "32nd",
-        answer: "a",
+        choices: ["1st", "16th", "3rd", "32nd"],
+        answer: "1st",
     },
     {
         question: "Who has the MOST ammount of points in Bruins history?",
-        a: "Chara",
-        b: "Orr",
-        c: "Espisito",
-        d: "Borque",
-        answer: "d",
+        choices: ["Chara", "Orr", "Espisito", "Borque"],
+        answer: "Borque",
     },
     {
         question: "What coding language allows you to style the webpage?",
-        a: "I don't speak coding",
-        b: "Chara",
-        c: "Css",
-        d: "HTML",
-        answer: "c",
+        choices: ["I don't speak coding", "Chara", "CSS", "HTML"],
+        answer: "CSS",
     }
 ];
 
-
+var questionIndex = 0;
 startBtn.addEventListener("click", startGame);
-quizBtn.addEventListener("click", quizInput);
+//quizBtn.addEventListener("click", quizInput);
 scoreBtn.addEventListener("click", leaderboard);
 newGameBtn.addEventListener("click", newGame);
 
@@ -63,20 +48,28 @@ function startGame () {
     let startGameArea = document.getElementById("startGameArea");
     quizArea.classList.toggle("displayNone");
     startGameArea.classList.toggle("displayNone");
+    showQuestion();
+}
+
+function showQuestion() {
+    question.innerHTML = quizQs[questionIndex].question;
+    for (var i = 0; i < quizQs[questionIndex].choices.length; i++) {
+        var btn = document.createElement("button");
+        btn.innerHTML = quizQs[questionIndex].choices[i];
+        document.getElementById("quizArea").appendChild(btn);
+        btn.addEventListener("click", function() {
+            console.log("hello");
+            questionIndex++;
+            showQuestion();
+        })
+    }
 }
 
 function quizInput () {
     let quizArea = document.getElementById("quizArea");
     let postGameArea = document.getElementById("postGame");
-    for(var i =0; i < quizQs.length; i++) {
-        userInput = quizQs[i].question; 
-        if(userInput == quizQs[i].answer){
-            score++;
-        }else {
-            quizArea.classList.toggle("displayNone");
-            postGameArea.classList.toggle("displayNone");
-        }
-    }
+    quizArea.classList.toggle("displayNone");
+    postGameArea.classList.toggle("displayNone");
 
 }
 
